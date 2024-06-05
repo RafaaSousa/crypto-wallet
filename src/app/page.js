67342661c30788fb8ef09decd42317cb2d810700
RafaaSousa <rafael.p.sousa@outlook.com>
2024-06-05 16:42:11 -0300

    Aplicando logica de programação na renderização das paginas de login.

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,109 @@
+"use client"
+
 import Input from '@/app/components/Input'
 import Button from '@/app/components/Button'
 import FormLayout from './components/FormLayout'
+import { useState } from 'react'
 
 import './home.css'
 
 
 export default function Home() {
+  const [isSignIn, setIsSignIn] = useState(false)
+  const [isForgotPassword, setIsForgotPassword] = useState(false)
+  const [isPasswordResetCompleted, setIsPasswordResetCompleted] = useState(false)
+
+  const handlePasswordResetSubmit = (e) => {
+    e.preventDefault();
+    setIsForgotPassword(false);
+    setIsPasswordResetCompleted(true);
+  }
   return (
     <div className='container'>
-      <FormLayout 
-        title="Login 👋" 
-        subtitle="controle sua carteira cripto sem burocracias">
-          <Button className="btn_redesocias">
-                <img src='./images/google.png'/>
-                Entre com Google
-          </Button>
-          <Button className='btn_redesocias'>
-                <img src='./images/facebook.png'/>
-                Entre com Facebook
-          </Button>
+      {!isForgotPassword && !isPasswordResetCompleted ? (
+        <FormLayout 
+          title={isSignIn ? "Cadastro 👋" :"Login 👋"}
+          subtitle="controle sua carteira cripto sem burocracias">
+            <Button className="btn_redesocias">
+                  <img src='./images/google.png'/>
+                  {isSignIn ? "Me cadastrar com Google" :"Entre com Google"}
+            </Button>
+            <Button className='btn_redesocias'>
+                  <img src='./images/facebook.png'/>
+                  {isSignIn ? "Me cadastrar com Facebook" : "Entre com Facebook" }
+            </Button>
 
-          <span className='login__container_span'>ou entre com um email</span>
+            <hr />
 
-          <Input className="inputform" type="email" placeholder="Email" />
-          <Input className="inputform" type="password" placeholder="Senha" />
+            <span className='login__container_span'>
+              {isSignIn ? "ou preencha seus dados manualmentel" : "ou entre com um email"}
+            </span>
 
-          <span className='inputform_span'>Esqueceu a senha?</span>
+            <Input className="inputform" type="email" placeholder="Email" />
+            <Input className="inputform" type="password" placeholder="Senha" />
+            {isSignIn && <Input className="inputform" type="password" placeholder="Confirmação de Senha" />}
 
-          <Button className='btn_submit' type="submit">Entrar</Button>
-          <Button className='btn_submit btn_submit_white' type="submit">Não tenho cadastro</Button>
-      </FormLayout>
+            {!isSignIn && (
+              <Button 
+                className='inputform_span'
+                onClick={() => setIsForgotPassword(true)}
+              >
+                {isSignIn ? "" : "Esqueceu a senha?"}
+              </Button>
+            )}
+
+            <Button 
+              className='btn_submit' 
+              type="submit"
+            >
+              {isSignIn ? "Me cadastrar" : "Entrar"}
+            </Button>
+            <Button 
+              className='btn_submit btn_submit_white' 
+              type="button"
+              onClick={() => setIsSignIn(!isSignIn)}
+            >
+                {isSignIn ? "Fazer login" : "Não tenho cadastro"}
+            </Button>
+        </FormLayout>
+      ) : isForgotPassword && !isPasswordResetCompleted ? (
+        <FormLayout
+          title="Esqueceu sua senha?"
+          subtitle="Não se preocupe, nós recuperamos para você">
+
+              <Input className="inputform" type='email' placeholder="Email cadastrado" />
+              <Button 
+                className='btn_submit' 
+                type="button"
+                onClick={handlePasswordResetSubmit}
+              >
+                Enviar recuperação de senha
+              </Button>
+            <Button
+              className='btn_submit btn_submit_white'
+              type="button"
+              onClick={() => setIsForgotPassword(false)}
+            >
+              Voltar para login
+            </Button>
+        </FormLayout>
+      ) : (
+        <FormLayout
+          title="Senha redefinida com Sucesso🎉"
+          subtitle="Agora volte para o login e entre com sua nova senha">
+
+            <Button
+              className='btn_submit'
+              type="button"
+              onClick={() => {
+                setIsForgotPassword(false);
+                setIsPasswordResetCompleted(false);
+              }}
+            >
+              Fazer login
+            </Button>
+        </FormLayout>
+      )}
     </div>
   );
 }
